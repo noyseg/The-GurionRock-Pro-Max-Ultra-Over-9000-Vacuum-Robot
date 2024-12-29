6d962efab2de6dd036917bd5a9bcb1a36256b778
noyseg <n3segal@gmail.com>
2024-12-29 08:55:06 +0200

    Start Fusion Slum

--- src/main/java/bgu/spl/mics/application/services/FusionSlamService.java
+++ src/main/java/bgu/spl/mics/application/services/FusionSlamService.java
@@ -1,7 +1,13 @@
 package bgu.spl.mics.application.services;
 
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.CrashedBroadcast;
+import bgu.spl.mics.application.messages.PoseEvent;
+import bgu.spl.mics.application.messages.TerminatedBroadcast;
+import bgu.spl.mics.application.messages.TickBroadcast;
+import bgu.spl.mics.application.messages.TrackedObjectsEvent;
 import bgu.spl.mics.application.objects.FusionSlam;
+import bgu.spl.mics.application.objects.TrackedObject;
 
 /**
  * FusionSlamService integrates data from multiple sensors to build and update
@@ -11,13 +17,14 @@ import bgu.spl.mics.application.objects.FusionSlam;
  * transforming and updating the map with new landmarks.
  */
 public class FusionSlamService extends MicroService {
+    
     /**
      * Constructor for FusionSlamService.
      *
      * @param fusionSlam The FusionSLAM object responsible for managing the global map.
      */
     public FusionSlamService(FusionSlam fusionSlam) {
-        super("Change_This_Name");
+        super("FusionSlam");
         // TODO Implement this
     }
 
@@ -28,6 +35,24 @@ public class FusionSlamService extends MicroService {
      */
     @Override
     protected void initialize() {
+        System.out.println(getName() + " started");
+        subscribeBroadcast(TickBroadcast.class, tick -> {
+
+
+        });
+        subscribeEvent(TrackedObjectsEvent.class, trackedObj -> { 
+
+        });
+        subscribeEvent(PoseEvent.class, pose -> { 
+
+        });
+        subscribeBroadcast(TerminatedBroadcast.class, terminate -> { 
+
+        });
+        subscribeBroadcast(CrashedBroadcast.class, crash -> { 
+
+        });
+
         // Subscribes to TickBroadcast, TrackedObjectsEvent, PoseEvent, TerminatedBroadcast,CrashedBroadcast
     }
 }
