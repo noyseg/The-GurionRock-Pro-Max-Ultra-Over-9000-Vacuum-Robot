19aac4aceabf2d92df4716e9361bb21457997b15
noamtamari <noamtamari98@gmail.com>
2024-12-31 13:48:41 +0200

    merging

---
+++ src/test/java/bgu/spl/mics/application/objects/CameraTest.java
@@ -0,0 +1,12 @@
+package bgu.spl.mics.application.objects;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CameraTest {
+
+    @Test
+    void getDetectedObjectsList() {
+    }
+}
